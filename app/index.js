975fe32a39b28332f0be2218f7efa773a5658d88
Sudhanshu <Sudhanshu@gmail.com>
2026-01-16 14:45:29 +0530

    Secret password added in index.js

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const AWS_SECRET_ACCESS_KEY = "abcdEFGHijklMNOPqrstUVWXyz1234567890AB";
+const PASSWORD = "password=MySuperSecretPassword123!";
 
 // DB config from ENV (IMPORTANT)
 const pool = new Pool({
