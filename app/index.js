33c84d9a9f6d33d6f5175734e93832ffabde0154
Sudhanshu <Sudhanshu@gmail.com>
2026-01-16 15:18:44 +0530

    Remove hardcoded secret; use env var

--- app/index.js
+++ app/index.js
@@ -4,8 +4,7 @@ const { Pool } = require("pg");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const AWS_SECRET_ACCESS_KEY = "abcdEFGHijklMNOPqrstUVWXyz1234567890AB";
-const PASSWORD = "password=MySuperSecretPassword123!";
+const PASSWORD = process.env.APP_PASSWORD;
 
 // DB config from ENV (IMPORTANT)
 const pool = new Pool({
